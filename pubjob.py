53e0ac2758554dd9561c841b4fad68c7c2b2bf5e
huxuanchenxy <wudianchatou@aliyun.com>
2022-10-09 14:38:44 +0800

    1.日志功能 2.相对稳定发布到mqtt不丢失版本

--- pubjob.py
+++ pubjob.py
@@ -4,14 +4,25 @@ import time
 from datetime import datetime
 import paho.mqtt.client as mqtt
 import json
-from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background  import BlockingScheduler
+import logging
 from apscheduler.triggers.cron import CronTrigger
 
 
 class Device:
     name = 'mydevice'
     time = datetime.now()
+now = datetime.now()
+fname = now.strftime('%Y-%m-%d') + '.log'
 
+logging.basicConfig(level=logging.INFO,#控制台打印的日志级别
+                    filename=fname,
+                    filemode='a',##模式，有w和a，w就是写模式，每次都会重新写日志，覆盖之前的日志
+                    #a是追加模式，默认如果不写的话，就是追加模式
+                    format=
+                    '%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s'
+                    #日志格式
+                    )
 
 
 HOST = "47.101.220.2"
@@ -27,8 +38,9 @@ def test():
     dev1.name = 'step1'
     dev1.time = datetime.now()
     jsonstr = json.dumps(dev1.__dict__, default=str)
-    client.publish(topic,jsonstr,0)
-    print("第一步：", datetime.now())
+    client.publish(topic,jsonstr,1)
+    #print("第一步：", datetime.now())
+    logging.info('第一步')
     time.sleep(10)
 
 
@@ -40,8 +52,9 @@ def test():
     dev2.name = 'step2'
     dev2.time = datetime.now()
     jsonstr2 = json.dumps(dev2.__dict__, default=str)
-    client2.publish(topic,jsonstr2,0)
-    print("第二步：", datetime.now())
+    client2.publish(topic,jsonstr2,1)
+    #print("第二步：", datetime.now())
+    logging.info('第二步')
     #client.loop_forever()
 
 def dojob():
