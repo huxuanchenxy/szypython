52e086d8eaf43b9ab214574c0ddfd71823ebe984
huxuanchenxy <wudianchatou@aliyun.com>
2023-08-21 15:58:45 +0800

    宝可梦原图下载

--- ptcg/download.py
+++ ptcg/download.py
@@ -1,13 +1,16 @@
 import requests
 
 
-count = 8734
+count = 155
 
-while count <= 8804:
+while count <= 166:
     print(count)
     
-    url = ("https://asia.pokemon-card.com/hk/card-img/hk0000{}.png").format(count)  # Replace with the URL of the image you want to download
-    filename = ("hk0000{}.png").format(count)  # Replace with the desired filename for the downloaded image
+    # url = ("https://tcg.pokemon.com/assets/img/expansions/lost-origin/cards/en-us/SWSH11_EN_TG_{}-2x.jpg").format(count)  # Replace with the URL of the image you want to download
+    # url = ("https://asia.pokemon-card.com/hk/card-img/hk0000{}.png").format(count) 
+    url = ("https://limitlesstcg.nyc3.digitaloceanspaces.com/tpci/SSP/SSP_{}_R_EN.png").format(count) 
+    
+    filename = ("SSP_{}_R_EN.png").format(count)  # Replace with the desired filename for the downloaded image
     # https://asia.pokemon-card.com/hk/card-img/hk00008867.png
     response = requests.get(url)
     response.raise_for_status()
